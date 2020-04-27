81f19d8389f50e29f12c9fe6b6b84c62aac09f94
gaoxiwei <15871707696@163.com>
2020-04-27 16:40:57 +0800

    利用self修改了源代码

--- task/central_index.py
+++ task/central_index.py
@@ -4,24 +4,25 @@
 # 解释:
 # 索引3 (nums[3] = 6) 的左侧数之和(1 + 7 + 3 = 11)，与右侧数之和(5 + 6 = 11)相等。
 # 同时, 3 也是第一个符合要求的中心索引。
+class A():
+    def pivotIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums_len = len(nums)
+        sum_nums = sum(nums)
+        sum_left = 0
+        for i in range(nums_len):
+            if i >= 1:
+                sum_left += nums[i - 1]
+            sum_right = sum_nums - sum_left - nums[i]
+            if sum_left == sum_right:
+                return i, nums[i]
+        return -1, -1
 
-def pivotIndex(nums):
-    """
-    :type nums: List[int]
-    :rtype: int
-    """
-    nums_len = len(nums)
-    sum_nums = sum(nums)
-    sum_left = 0
-    for i in range(nums_len):
-        if i >= 1:
-            sum_left +=nums[i - 1]
-        sum_right = sum_nums - sum_left - nums[i]
-        if sum_left == sum_right:
-            return i,nums[i]
-    return -1, -1
 
 
 if __name__ == '__main__':
     nums = [1, 7, 3, 6, 5, 6]
-    index, central = pivotIndex(nums)
+    index, central = A().pivotIndex(nums)
